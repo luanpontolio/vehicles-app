02d8e2ae911dad0bf7793544717b73a95005073c
Luan Pontolio <luan.pontolio@gmail.com>
2017-02-04 18:52:38 +0000

    Adds news components.

---
+++ src/js/components/alert-message.component.js
@@ -0,0 +1,10 @@
+let AlertMessage = {
+  bindings: {
+  	condition: '=',
+    message: '='
+  },
+
+  templateUrl: 'components/alert-message.html'
+};
+
+export default AlertMessage;
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -5,4 +5,8 @@ let componentsModule = angular.module('app.components', []);
 import SearchInput from './search-input.component';
 componentsModule.component('searchInput', SearchInput);
 
+
+import AlertMessage from './alert-message.component';
+componentsModule.component('alertMessage', AlertMessage);
+
 export default componentsModule;
